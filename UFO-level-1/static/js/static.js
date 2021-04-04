32dad14c0eaca60c277714a812782915c2739eb1
eddie0rNot <eddie.buckalew@gmail.com>
2021-04-04 18:47:04 -0500

    last push

--- UFO-level-1/static/js/static.js
+++ UFO-level-1/static/js/static.js
@@ -1,7 +1,9 @@
+function defTable(){  
+
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log from data.js
 console.log(data);
 
 // Step 5: Use d3 to update each cell's text with
@@ -14,6 +16,49 @@ console.log(data);
 // Append a cell to the row for each value
 // in the weather report object
      var cell = row.append("td");
-     cell.text(value);
+     cell.html(value);
    });
  });
+};
+ // Select the form
+
+// Complete the event handler function for the form
+function runEnter(){
+    var sightings = data;
+
+    var form = d3.select("#dateInput");
+    var button = d3.select("#filter-btn");
+  
+  // Create event handlers 
+    button.on("click", runEnter);
+    form.on("submit", runEnter);
+  
+  // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#dateInput");
+
+  // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(sightings);
+
+    var tbody = d3.select("tbody");
+
+    var filteredData = data.filter(sighting => sighting.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    filteredData.forEach(function(filteredData) {
+      console.log(filteredData);
+      var row = tbody.append("tr");
+      Object.entries(filteredData).forEach(function([key, inputValue]) {
+        console.log(key, inputValue);
+  // Append a cell to the row for each value
+  // in the weather report object
+        var cell = row.append("td");
+        cell.html(inputValue);
+    });
+  });
+};
+ 
+
